29e505456b74a4ed0919c8466e6ba2e262a31cac
Hubert Podlewski <h.podlewski@stud.elka.pw.edu.pl>
2022-04-18 22:35:05 +0200

    Added automatic key generation

--- start.py
+++ start.py
@@ -1,9 +1,17 @@
 from app.db_model import db, URLMapping
 from app import create_app
+from uuid import uuid4
 
 app = create_app(db)
-with open("key.txt", "r") as fh:
+with open("key.txt", "w+") as fh:
     app.secret_key = fh.readline()
+    if not app.secret_key:
+        key = str(uuid4()).replace('-', '')
+        app.secret_key = key
+        
+        fh.truncate(0)
+        fh.write(key)
+
 
 
 if __name__ == '__main__':
