7cf97e613b60567addb4495da9b3ad0242cbd897
Hubert Podlewski <h.podlewski@stud.elka.pw.edu.pl>
2022-04-30 19:57:49 +0200

    Added base for User table

--- app/db_model.py
+++ app/db_model.py
@@ -1,6 +1,21 @@
 from flask_sqlalchemy import SQLAlchemy
+from hashlib import pbkdf2_hmac as hash
+from os import urandom
 
 db = SQLAlchemy()
+hash_iterations = 50_000
+
+class User(db.Model):
+    id = db.Column(db.INT, primary_key=True)
+    name = db.Column(db.String(20), nullable=False)
+    password_hash = db.Column(db.String(64), nullable=False)
+    password_salt = db.Column(db.String(32), nullable=False)
+
+    def __init__(self, name, passwd):
+        self.name = name
+        self.password_salt = urandom(32)
+        self.password_hash = hash('sha256', bytes(passwd, 'utf-8'), self.password_hash, hash_iterations)
+
 
 class URLMapping(db.Model):
     custom_url = db.Column(db.String(50), primary_key=True)
