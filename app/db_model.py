3824e1a7f259dceda5e044d0eae4607fee274cf6
Hubert Podlewski <h.podlewski@stud.elka.pw.edu.pl>
2022-04-30 21:37:52 +0200

    Added user - mapping relationship; fixed a bug where the application generated a new key despite one already being present in the file

--- app/db_model.py
+++ app/db_model.py
@@ -2,13 +2,17 @@ from flask_sqlalchemy import SQLAlchemy
 from hashlib import pbkdf2_hmac as hash
 from os import urandom
 
+
 db = SQLAlchemy()
 hash_iterations = 50_000
 
 class User(db.Model):
+    __tablename__ = 'user'
     name = db.Column(db.String(20), primary_key=True)
     password_hash = db.Column(db.String(64), nullable=False)
     password_salt = db.Column(db.String(32), nullable=False)
+
+    mappings = db.relationship("URLMapping")
 
     def __init__(self, name, passwd):
         self.name = name
@@ -17,12 +21,15 @@ class User(db.Model):
 
 
 class URLMapping(db.Model):
+    __tablename__ = 'mapping'
     custom_url = db.Column(db.String(50), primary_key=True)
     target_url = db.Column(db.String(100), nullable=False)
 
+    owner = db.Column(db.String(20), db.ForeignKey('user.name'))
+
     def __init__(self, custom, target):
-            self.custom_url = custom
-            self.target_url = target
+        self.custom_url = custom
+        self.target_url = target
 
     def __repr__(self):
         return '<%r -> %r>' % (self.custom_url, self.target_url)
