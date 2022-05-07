7ea63dcea7358def87b8db739e2b28ebd6ddcb3a
Hubert Podlewski <h.podlewski@stud.elka.pw.edu.pl>
2022-05-07 23:12:21 +0200

    Changed register endpoint to utilize wtforms

--- app/login/routes.py
+++ app/login/routes.py
@@ -3,20 +3,22 @@ import flask_login as flogin
 
 from . import login
 from .login_manager import *
-from .forms import LoginForm
+from .forms import LoginForm, RegisterForm
 
 
 @login.route('/register', methods=['GET', 'POST'])
 def register():
-        if flask.request.method == 'POST':
-            nickname = (flask.request.form.get('nickname'))
-            password = (flask.request.form.get('password'))
+    form = RegisterForm()
+    if flask.request.method == 'POST':
+        if form.validate_on_submit():
+            nickname = form.username.data
+            password = form.password.data
             if register_user(nickname, password):
                 return flask.redirect('/')
             else:
                 flask.flash("User already exists.")
 
-        return flask.render_template('login/register.html')
+    return flask.render_template('register.html', form=form)
 
 @login.route('/login', methods=['GET', 'POST'])
 def login_page():
